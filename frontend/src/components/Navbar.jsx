d26a2e1e3a9f1c3dde217eddc8eec41c72c09b1d
Precious-100 <preciousmotsamai16.com>
2025-01-10 02:36:09 +0200

    Updated Navbar, App, and index files

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,2 +1,23 @@
 import React from 'react'
-import '/.Navbar.css'
+import './Navbar.css'
+import logo from '../assets/logo.png';
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={logo} alt=''/>
+      <ul className='navbar-menu'>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-App</li>
+        <li>Contact us</li>
+
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
+
